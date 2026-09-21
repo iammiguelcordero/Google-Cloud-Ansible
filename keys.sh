e8e153a08e6dc3bd0f0808a88cd5ba4cb6d51206
miguelcordero <iammiguelcordero@gmail.com>
2026-09-21 12:29:58 -0700

    Datadog agent

--- keys.sh
+++ keys.sh
@@ -6,10 +6,14 @@ cat ~/.ssh/id_ed25519.pub
 #Create the folder and add the permisions
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICHrGrwPVaImURv0FWMrrJHZkqOfZS0BWNQsrSNCe7iU student-01-47e4cc155a71@vm-ansible-001" >> ~/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOIhTZBFc+kLzPg7cSU3o3yYIk70v/tCR/OKt196jPA5 student-04-4242e59036a8@vm-ansible-001" >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
 #Done, the connection is sucessfull, test it on vm-ansible
 ssh <your user>@<ip-destiny>
 
 ansible-galaxy install geerlingguy.nginx -p roles/
+
+ansible all -i inventory.ini -m ping
+
+ansible-playbook -i inventory.ini site.yml
